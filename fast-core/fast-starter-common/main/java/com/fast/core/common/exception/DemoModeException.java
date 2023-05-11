5b453e23aba64fe358800fdf29ea149cb8506447
黄嘉浩 <1300286201@qq.com>
2023-05-11 15:05:15 +0800

    init

---
+++ fast-core/fast-starter-common/main/java/com/fast/core/common/exception/DemoModeException.java
@@ -0,0 +1,15 @@
+package com.fast.core.common.exception;
+
+/**
+ * 演示模式异常
+ * 
+ * @author @Dog_Elder
+ */
+public class DemoModeException extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+
+    public DemoModeException()
+    {
+    }
+}
