11e126079a89ab703f5bc99a4ddb7394e784441c
黄嘉浩 <1300286201@qq.com>
2023-06-02 17:46:25 +0800

    feat:全局异常处理器

---
+++ fast-core/fast-core-boot/src/main/java/com/fast/core/boot/web/exception/GlobalExceptionHandler.java
@@ -0,0 +1,139 @@
+package com.fast.core.boot.web.exception;
+
+import cn.dev33.satoken.exception.NotLoginException;
+import cn.dev33.satoken.exception.NotPermissionException;
+import cn.dev33.satoken.util.SaResult;
+import com.fast.core.common.domain.domain.R;
+import com.fast.core.common.exception.BaseException;
+import com.fast.core.common.exception.CustomException;
+import com.fast.core.common.util.Util;
+import com.fast.core.common.util.WebUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.MultipartException;
+
+import javax.security.auth.login.AccountExpiredException;
+import java.util.List;
+
+/**
+ * 全局异常处理器
+ *
+ * @author @Dog_Elder
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+//    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    /**
+     * 基础异常
+     */
+    @ExceptionHandler(BaseException.class)
+    public R baseException(BaseException e) {
+        runLog(e);
+        return R.error(e.getMessage());
+    }
+
+    /**
+     * 文件上传异常
+     */
+    @ExceptionHandler(MultipartException.class)
+    public R handleBusinessException(MaxUploadSizeExceededException e) {
+        runLog(e);
+        return R.error("上传文件失败");
+    }
+
+    /**
+     * 认证异常
+     **/
+    @ExceptionHandler(NotLoginException.class)
+    public R handlerException(NotLoginException e) {
+        runLog(e);
+        String type = e.getType();
+        switch (type){
+            case NotLoginException.NOT_TOKEN:
+            case NotLoginException.INVALID_TOKEN:
+            case NotLoginException.TOKEN_TIMEOUT:
+                return R.error(R.Type.UNAUTHORIZED);
+            case NotLoginException.BE_REPLACED:
+                return R.error(R.Type.USER_DISCONNECTED);
+            case NotLoginException.KICK_OUT:
+                return R.error(R.Type.USER_COMPULSION_LINE);
+        }
+        return R.error(R.Type.UNAUTHORIZED);
+    }
+
+    /**
+     * 未授权异常
+     **/
+    @ExceptionHandler(NotPermissionException.class)
+    public R handlerException(NotPermissionException e) {
+        runLog(e);
+        return R.error(R.Type.FORBIDDEN);
+    }
+
+    /**
+     * 业务异常
+     */
+    @ExceptionHandler(CustomException.class)
+    public R businessException(CustomException e) {
+        runLog(e);
+        if (Util.isNotNull(e.getCode())) {
+            return R.error(e.getMessage());
+        }
+        return R.error(e.getMessage());
+    }
+
+
+    @ExceptionHandler(AccountExpiredException.class)
+    public R handleAccountExpiredException(AccountExpiredException e) {
+        runLog(e);
+        return R.error(e.getMessage());
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public R handleException(Exception e) {
+        runLog(e);
+        return R.error(e.getMessage());
+    }
+
+    /**
+     * 自定义验证异常
+     */
+    @ExceptionHandler(BindException.class)
+    public R validatedBindException(BindException bindException) {
+        runLog(bindException);
+        Object target = bindException.getTarget();
+        FieldError fieldError = bindException.getFieldError();
+        return WebUtil.dealError(target, fieldError);
+    }
+
+    /**
+     * 入参校验异常
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public R validExceptionHandler(MethodArgumentNotValidException mde) {
+        runLog(mde);
+        Object target = mde.getBindingResult().getTarget();
+        List<ObjectError> errors = mde.getBindingResult().getAllErrors();
+        FieldError fieldError = errors.isEmpty() ? null : (FieldError) errors.get(0);
+        return WebUtil.dealError(target, fieldError);
+    }
+
+    public void runLog(Exception e) {
+        StackTraceElement stackTraceElement = e.getStackTrace()[0];
+        String className = stackTraceElement.getClassName();
+        String fileName = stackTraceElement.getFileName();
+        int lineNumber = stackTraceElement.getLineNumber();
+        String methodName = stackTraceElement.getMethodName();
+        log.error("类名:{},文件名:{},行数:{},方法名:{}", className, fileName, lineNumber, methodName);
+        log.error(e.getMessage(), e);
+    }
+}
