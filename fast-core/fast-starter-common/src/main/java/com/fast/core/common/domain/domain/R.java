c9e6a13a3ed6bae7b27f9645b4ce87eedf918f2e
黄嘉浩 <1300286201@qq.com>
2023-06-02 17:47:27 +0800

    perf:消息返回体

--- fast-core/fast-starter-common/src/main/java/com/fast/core/common/domain/domain/R.java
+++ fast-core/fast-starter-common/src/main/java/com/fast/core/common/domain/domain/R.java
@@ -8,142 +8,101 @@ import java.io.Serializable;
  *
  * @param <T> 数据类型
  */
+/**
+ * 操作消息提醒
+ *
+ * @param <T> 数据类型
+ */
 public class R<T extends Serializable> {
-    /** 状态码 */
     private int code;
-    /** 返回内容 */
     private String msg;
-    /** 数据对象 */
     private T data;
 
-    /**
-     * 状态类型
-     */
     public enum Type {
-        /** 成功 */
-        SUCCESS(200),
-        /** 错误 */
-        ERROR(500);
+        SUCCESS(200, "成功"),
+        CREATED(201, "已创建"),
+        ACCEPTED(202, "已接受"),
+        BAD_REQUEST(400, "错误的请求"),
+        UNAUTHORIZED(401, "未经授权"),
+        FORBIDDEN(403, "禁止访问"),
+        NOT_FOUND(404, "未找到"),
+        SERVER_ERROR(500, "服务器错误"),
+        USER_COMPULSION_LINE(1002, "该账号已已被强行下线"),
+        USER_DISCONNECTED(1001, "该账号已在别处登录");
 
         private final int value;
+        private final String description;
 
-        Type(int value) {
+        Type(int value, String description) {
             this.value = value;
+            this.description = description;
         }
 
         public int value() {
             return this.value;
         }
+
+        public String description() {
+            return this.description;
+        }
     }
 
-    /**
-     * 初始化一个新创建的 R 对象，使其表示一个空消息。
-     */
     public R() {
     }
 
-    /**
-     * 初始化一个新创建的 R 对象
-     *
-     * @param type 状态类型
-     * @param msg 返回内容
-     */
-    public R(Type type, String msg) {
-        this.code = type.value();
-        this.msg = msg;
-    }
-
-    /**
-     * 初始化一个新创建的 R 对象
-     *
-     * @param type 状态类型
-     * @param msg 返回内容
-     * @param data 数据对象
-     */
     public R(Type type, String msg, T data) {
         this.code = type.value();
         this.msg = msg;
         this.data = data;
     }
 
-    /**
-     * 返回成功消息
-     *
-     * @return 成功消息
-     */
+    public R(Type type, String msg) {
+        this.code = type.value();
+        this.msg = msg;
+    }
+    public R(Type type) {
+        this.code = type.value();
+        this.msg = type.description();
+    }
+
     public static <T extends Serializable> R<T> success() {
-        return R.success("操作成功");
+        return R.success("操作成功", null);
     }
 
-    /**
-     * 返回成功数据
-     *
-     * @return 成功消息
-     */
-    public static <T extends Serializable> R<T> success(T data) {
-        return R.success("操作成功", data);
-    }
-
-    /**
-     * 返回成功消息
-     *
-     * @param msg 返回内容
-     * @return 成功消息
-     */
     public static <T extends Serializable> R<T> success(String msg) {
         return R.success(msg, null);
     }
 
-    /**
-     * 返回成功消息
-     *
-     * @param msg 返回内容
-     * @param data 数据对象
-     * @return 成功消息
-     */
+    public static <T extends Serializable> R<T> success(T data) {
+        return R.success("操作成功", data);
+    }
+
     public static <T extends Serializable> R<T> success(String msg, T data) {
         return new R<>(Type.SUCCESS, msg, data);
     }
 
-    /**
-     * 返回错误消息
-     *
-     * @return 错误消息
-     */
     public static <T extends Serializable> R<T> error() {
-        return R.error("操作失败");
+        return R.error("操作失败", null);
     }
 
-    /**
-     * 返回错误消息:版本不一致
-     *
-     * @return 错误消息
-     */
     public static <T extends Serializable> R<T> errorVersion() {
-        return R.error("操作失败,版本不一致!");
+        return R.error("操作失败,版本不一致!", null);
     }
 
-    /**
-     * 返回错误消息
-     *
-     * @param msg 返回内容
-     * @return 错误消息
-     */
     public static <T extends Serializable> R<T> error(String msg) {
         return R.error(msg, null);
     }
 
-    /**
-     * 返回错误消息
-     *
-     * @param msg 返回内容
-     * @param data 数据对象
-     * @return 错误消息
-     */
-    public static <T extends Serializable> R<T> error(String msg, T data) {
-        return new R<>(Type.ERROR, msg, data);
+    public static <T extends Serializable> R<T> error(Type type) {
+        return new R<>(type);
+    }
+    public static <T extends Serializable> R<T> error(Type type,String msg) {
+        return new R<>(type,msg);
     }
 
+    public static <T extends Serializable> R<T> error(String msg, T data) {
+        return new R<>(Type.SERVER_ERROR, msg, data);
+    }
 
     public int getCode() {
         return code;
