67cd4d8249efd9535c4998026f811e0f9fdcca1c
黄嘉浩 <1300286201@qq.com>
2023-06-06 01:22:11 +0800

    test:测试接口

--- fast-service/fast-service-common/src/main/java/com/fast/common/controller/TestController.java
+++ fast-service/fast-service-common/src/main/java/com/fast/common/controller/TestController.java
@@ -1,19 +1,16 @@
 package com.fast.common.controller;
 
-import cn.dev33.satoken.stp.SaTokenInfo;
-import cn.hutool.json.JSONUtil;
+import cn.dev33.satoken.annotation.SaCheckPermission;
 import com.fast.common.entity.sys.SysSet;
 import com.fast.common.service.ISysSetService;
 import com.fast.core.common.domain.domain.R;
 import com.fast.core.common.domain.page.TableDataInfo;
 import com.fast.core.safe.annotation.manage.ManageCheckLogin;
 import com.fast.core.safe.annotation.manage.ManageCheckPermission;
-import com.fast.core.safe.util.ManageUtil;
 import com.fast.core.util.FastRedis;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -45,7 +42,7 @@ public class TestController extends BaseController {
      */
     @GetMapping
     @RequestMapping("/set")
-    @ManageCheckPermission("user.add")
+    @SaCheckPermission("user.add")
     public R<TableDataInfo> list(SysSet sysSet) {
         startPage();
         List<SysSet> list = sysSetService.list(sysSet);
@@ -67,31 +64,5 @@ public class TestController extends BaseController {
     @GetMapping("/set3/{v2}")
     public R<String> list3(@RequestParam("v1")String aa,@PathVariable("v2") String v2) {
         return R.success(aa+"   "+v2);
-    }
-
-    @PostMapping("/login")
-    public R doLogin(@RequestParam("username")String username,@RequestParam("password")String password) {
-        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
-        if("zhang".equals(username) && "123456".equals(password)) {
-            ManageUtil.login(10001);
-            // 第2步，获取 Token  相关参数
-            SaTokenInfo tokenInfo = ManageUtil.getTokenInfo();
-            // 第3步，返回给前端
-            return R.success(JSONUtil.parseObj(tokenInfo));
-        }
-        return R.success("登录失败");
-    }
-    @PostMapping("/ee")
-    @ResponseBody
-    public R<String> ee(@RequestParam("file") MultipartFile[] file) {
-        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
-        System.out.println(file);
-        return R.success("登录失败");
-    }
-
-    // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
-    @RequestMapping("isLogin")
-    public String isLogin() {
-        return "当前会话是否登录：" + ManageUtil.isLogin();
     }
 }
