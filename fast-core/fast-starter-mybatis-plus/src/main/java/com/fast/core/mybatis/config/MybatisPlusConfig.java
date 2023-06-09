7599c44cf0f9e48173bbb9765bef75631783a822
黄嘉浩 <1300286201@qq.com>
2023-06-10 01:41:20 +0800

    feat:mybatis

--- fast-core/fast-starter-mybatis-plus/src/main/java/com/fast/core/mybatis/config/MybatisPlusConfig.java
+++ fast-core/fast-starter-mybatis-plus/src/main/java/com/fast/core/mybatis/config/MybatisPlusConfig.java
@@ -1,23 +1,26 @@
 package com.fast.core.mybatis.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @description: mybatis拦截器,目前用于乐观锁
+ * @description: mybatis配置
  **/
 @Configuration
 public class MybatisPlusConfig {
     /**
-     * mybatis拦截器,目前用于乐观锁
+     * mybatis拦截器
      */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
         mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        mybatisPlusInterceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
         return mybatisPlusInterceptor;
     }
+
 
 }
---
+++ fast-core/fast-starter-mybatis-plus/src/main/java/com/fast/core/mybatis/dao/BaseDao.java
@@ -0,0 +1,7 @@
+package com.fast.core.mybatis.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface BaseDao<T> extends BaseMapper<T> {
+
+}
