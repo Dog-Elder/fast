ad00803adff661d612a1ae92c45f4435c0e5266c
黄嘉浩 <1300286201@qq.com>
2023-07-12 00:06:30 +0800

    perf:增加公共字段常量

---
+++ fast-core/fast-starter-mybatis-plus/src/main/java/com/fast/core/mybatis/constant/PublicFieldConstant.java
@@ -0,0 +1,23 @@
+package com.fast.core.mybatis.constant;
+
+/**
+ * 公共字段常量
+ **/
+public class PublicFieldConstant {
+    /**
+     * 创建者
+     */
+    public static final String CREATE_BY = "createBy";
+    /**
+     * 创建时间
+     */
+    public static final String CREATE_TIME = "createTime";
+    /**
+     * 更新者
+     */
+    public static final String UPDATE_BY = "updateBy";
+    /**
+     * 更新时间
+     */
+    public static final String UPDATE_TIME = "updateTime";
+}
