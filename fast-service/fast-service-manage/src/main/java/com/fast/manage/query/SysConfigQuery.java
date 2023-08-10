93d4e221fc3bf2557496e2a93d166d28a6713015
黄嘉浩 <1300286201@qq.com>
2023-08-10 11:53:35 +0800

    feat:系统配置

---
+++ fast-service/fast-service-manage/src/main/java/com/fast/manage/query/SysConfigQuery.java
@@ -0,0 +1,46 @@
+package com.fast.manage.query;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.fast.core.common.domain.page.Query;
+import com.fast.core.common.validate.annotation.Display;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 系统参数配置查询
+ *
+ * @author 黄嘉浩 1300286201@qq.com
+ * @since 1.0.0 2023-08-09
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class SysConfigQuery extends Query {
+    /**
+     * id
+     */
+    @Display("id")
+    @TableField("id")
+    private String id;
+
+    /**
+     * 参数名
+     */
+    @Display("参数名")
+    @TableField("param_name")
+    private String paramName;
+
+    /**
+     * 参数键
+     */
+    @Display("参数键")
+    @TableField("param_key")
+    private String paramKey;
+
+    /**
+     * 状态（1代表启用 0代表关闭）
+     */
+    @Display("状态（1代表启用 0代表关闭）")
+    @TableField("state")
+    private String state;
+
+}
