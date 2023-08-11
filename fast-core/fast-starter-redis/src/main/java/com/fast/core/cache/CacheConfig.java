af6932b5ff242c0376d4c54ac5e9ed03727a6366
黄嘉浩 <1300286201@qq.com>
2023-08-11 09:47:33 +0800

    fix:缓存配置

--- fast-core/fast-starter-redis/src/main/java/com/fast/core/cache/CacheConfig.java
+++ fast-core/fast-starter-redis/src/main/java/com/fast/core/cache/CacheConfig.java
@@ -1,30 +1,17 @@
 package com.fast.core.cache;
 
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.support.SimpleCacheManager;
-import org.springframework.cache.support.CompositeCacheManager;
-import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.cache.annotation.CachingConfigurer;
-import org.springframework.cache.annotation.CachingConfigurationSelector;
-import org.springframework.cache.interceptor.CacheResolver;
-import org.springframework.cache.interceptor.CacheErrorHandler;
-import org.springframework.cache.interceptor.KeyGenerator;
-import org.springframework.cache.interceptor.SimpleKeyGenerator;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.time.Duration;
-import java.util.Arrays;
 
 @Configuration
 @EnableCaching
@@ -45,11 +32,8 @@ public class CacheConfig extends CachingConfigurerSupport {
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         RedisCacheWriter cacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
-        RedisCacheConfiguration defaultCacheConfiguration = redisCacheConfiguration();
-
-        RedisCacheManager cacheManager = new RedisCacheManager(cacheWriter, defaultCacheConfiguration);
-
-        return cacheManager;
+        RedisCacheConfiguration defaultCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+        return new com.fast.core.cache.CacheManager(cacheWriter, defaultCacheConfiguration);
     }
 
     //  其他配置...
