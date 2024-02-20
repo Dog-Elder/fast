0745b06e820bbf7b3f9df035d3f0d66fe7fdd426
黄嘉浩 <huangjiahao@jk366.net>
2024-02-20 19:03:12 +0800

    feat: Netty

--- fast-core/fast-starter-netty/src/main/java/com/fast/core/netty/handler/NettyHandler.java
+++ fast-core/fast-starter-netty/src/main/java/com/fast/core/netty/handler/NettyHandler.java
@@ -1,8 +1,10 @@
 package com.fast.core.netty.handler;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +15,15 @@ public class NettyHandler extends SimpleChannelInboundHandler {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-        log.info("Netty接收到的消息:" + msg);
+        if (msg instanceof ByteBuf) {
+            ByteBuf byteBuf = (ByteBuf) msg;
+            String text = byteBuf.toString(CharsetUtil.UTF_8); // 将ByteBuf转换为字符串
+            log.info("Netty接收到的消息: {}", text);
+            // 在这里处理文本消息
+        } else {
+            // 如果消息不是ByteBuf类型，可以根据需要进行其他处理
+            log.warn("Netty接收到的消息不是ByteBuf类型: {}", msg);
+        }
     }
 }
 
